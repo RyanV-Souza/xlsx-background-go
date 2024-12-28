ab44db7deb46e86a845d120d39ea6a73cc384c7d
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2024-12-28 18:47:45 -0300

    feat: implements wagon repository

---
+++ internal/repository/wagon_repository.go
@@ -0,0 +1,26 @@
+package repository
+
+import (
+	"time"
+
+	"github.com/RyanV-Souza/xlsx-background-go/internal/model"
+	"gorm.io/gorm"
+)
+
+type WagonRepository struct {
+	db *gorm.DB
+}
+
+func NewWagonRepository(db *gorm.DB) *WagonRepository {
+	return &WagonRepository{db: db}
+}
+
+func (r *WagonRepository) GetByDateRange(startDate, endDate time.Time) ([]model.Wagon, error) {
+	var wagons []model.Wagon
+	result := r.db.Where("date BETWEEN ? AND ?", startDate, endDate).Find(&wagons)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return wagons, nil
+}
