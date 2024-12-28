c19ef53f7b14f256c06705fa8d6a2b4dccd52427
Ryan Vieira Souza <ryanvsouza.dev@gmail.com>
2024-12-28 18:45:07 -0300

    feat: implements use rrepository

---
+++ internal/repository/user_repository.go
@@ -0,0 +1,24 @@
+package repository
+
+import (
+	"github.com/RyanV-Souza/xlsx-background-go/internal/model"
+	"gorm.io/gorm"
+)
+
+type UserRepository struct {
+	db *gorm.DB
+}
+
+func NewUserRepository(db *gorm.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) GetById(id uint) (*model.User, error) {
+	var user model.User
+	result := r.db.First(&user, id)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	return &user, nil
+}
